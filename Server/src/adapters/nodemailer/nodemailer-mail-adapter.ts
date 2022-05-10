2326d45a52ef61a9e3016033745e44e53f057fb7
Breno <brenocal73@gmail.com>
2022-05-10 03:35:03 -0300

    feat: Finished backend aplication

---
+++ Server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import nodemailer from 'nodemailer';
+import { MailAdapter, SendMailData } from "../mail-adapter";
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "4ea7b78f19dc22",
+      pass: "d5fe9d50f7ceaa"
+    }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+    async sendMail({ subject, body }: SendMailData) {
+        await transport.sendMail({
+            from: 'Equipe Feedget <oi@feedget.com>',
+            to: 'Breno Costa <brenocolima@gmail.com',
+            subject,
+            html: body,
+        });
+    };
+}
